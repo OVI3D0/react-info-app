4c0da74d59a9392508a857781be25b1eaaa9c0c5
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-04-20 12:16:14 -0400

    commit from my macbook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world
+          this is a commmit from my macbook
         </p>
         <a
           className="App-link"
